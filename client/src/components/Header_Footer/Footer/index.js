e06e66c2bfab59a5dcc706dab6c297be471063a1
mr <roshahid@gmail.com>
2019-09-18 17:59:29 +0430

    second commit , i commited to test deploying to the server

---
+++ client/src/components/Header_Footer/Footer/index.js
@@ -0,0 +1,46 @@
+// import React from 'react';
+
+// import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+// // import faCompass from '@fortawesome/fontawesome-free-solid/faCompass';
+// import faPhone from '@fortawesome/fontawesome-free-solid/faPhone';
+// // import faClock from '@fortawesome/fontawesome-free-solid/faClock';
+// import faEnvelope from '@fortawesome/fontawesome-free-solid/faEnvelope';
+
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { far } from '@fortawesome/free-regular-svg-icons'
+// import { fas } from '@fortawesome/free-solid-svg-icons'
+// library.add(far, fas)
+
+// const Footer = () => {
+//     return (
+//         <footer className="bck_b_dark">
+//             <div className="container">
+//                 <div className="logo">
+//                     Serka
+//                 </div>
+//                 <div className="wrapper">
+//                     <div className="left">
+//                         <h2>Contact information</h2>
+//                         <div className="business_nfo">
+//                             <div className="tag">
+//                                 {/* <FontAwesomeIcon
+//                                     icon={fas fa-compass}
+//                                     className="icon"
+//                                 /> */}
+//                             </div>
+//                             <div className="nfo">
+//                                 <div>Address</div>
+//                                 <div>jalali Street no.105</div>
+//                             </div>
+//                         </div>
+//                     </div>
+//                     <div className="left">
+
+//                     </div>
+//                 </div>
+//             </div>
+//         </footer>
+//     );
+// };
+
+// export default Footer;
--- server/server.js
+++ server/server.js
@@ -19,11 +19,78 @@ mongoose.connect(DATABASE, { useNewUrlParser: true })
 const { User } = require('./models/user');
 const { Brand } = require('./models/brand');
 const { Wood } = require('./models/wood');
+const { Product } = require('./models/product');
 
 
 // Middleware 
 const { auth } = require('./middleware/auth');
 const { admin } = require('./middleware/admin');
+
+
+
+//===================================
+//              PRODUCTS
+//===================================
+
+// BY ARRIVAL
+// /articles?sortBy=createdAt&order=desc&limit=4
+
+// BY SELL
+// /articles?sortBy=sold&order=desc&limit=4
+app.get('/api/product/articles', (req, res) => {
+    let order = req.query.order ? req.query.order : 'asc';
+    let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+    let limit = req.query.limit ? parseInt(req.query.limit) : 100;
+
+    Product.find().
+        populate('brand').
+        populate('wood').
+        sort([[sortBy, order]]).
+        limit(limit).
+        exec((err, articles) => {
+            if (err) res.status(400).send(err);
+            res.send(articles)
+        })
+})
+
+app.get('/api/product/article_by_id', (req, res) => {
+    let type = req.query.type;
+    let item = req.query.id;
+
+    if (type == 'array') {
+        let ids = req.query.id.split(',');
+        items = [];
+        items = ids.map(item => {
+            return mongoose.Types.ObjectId(item);
+        })
+    }
+
+    Product.
+        find({ '_id': { $in: items } }).
+        populate('brand').
+        populate('wood').
+        exec((err, docs) => {
+            return res.status(200).send(docs);
+        })
+})
+
+
+
+
+app.post('/api/product/article', auth, admin, (req, res) => {
+    const product = new Product(req.body);
+
+    product.save((err, doc) => {
+        if (err) return res.json({ success: false, err });
+        res.status(200).json({
+            success: true,
+            article: doc
+        })
+    })
+})
+
+
+
 
 //===================================
 //              WOODS
